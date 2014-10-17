ec8e9eecc3d15de0028235f4df62b6cc7f98f263
Marcos Chavarría Teijeiro <chavarria1991@gmail.com>
2014-10-17 19:50:36 +0200

    Engadido esqueleto...

--- js/reveal_config.js
+++ js/reveal_config.js
@@ -1,12 +1,13 @@
 // Full list of configuration options available here:
 // https://github.com/hakimel/reveal.js#configuration
 Reveal.initialize({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
     controls: false,
     progress: true,
     history: true,
     center: true,
+    overview: true,
     theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
     transition: Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/none
     dependencies: [
