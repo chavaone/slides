f4bacc0df417826cebdc07f3f6c76f2316ea122f
Marcos Chavarría Teijeiro <chavarria1991@gmail.com>
2014-10-22 10:08:54 +0200

    Eliminar barra que fai que fallen os scripts e imaxes...

--- js/reveal_config.js
+++ js/reveal_config.js
@@ -9,7 +9,7 @@ Reveal.initialize({
     center: true,
     overview: true,
     theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
-    transition: Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/none
+    transition: Reveal.getQueryHash().transition || 'linear', // default/cube/page/concave/zoom/linear/none
     dependencies: [
         { src: 'reveal/lib/js/classList.js', condition: function() { return !document.body.classList; } },
         { src: 'reveal/plugin/markdown/showdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
@@ -17,6 +17,6 @@ Reveal.initialize({
         { src: 'reveal/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
         { src: 'reveal/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
         { src: 'reveal/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
-        { src: 'plugin/remotes/remotes.js', async: true, condition: function() { return !!document.body.classList; } }
+        { src: 'reveal/plugin/remotes/remotes.js', async: true, condition: function() { return !!document.body.classList; } }
         ]
 });
